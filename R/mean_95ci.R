e9195b3ee308cf82b5f6de8de267565f64f12c71
tommywht <tommyhtwong@gmail.com>
2022-04-10 09:53:47 +0800

    Update documentation for functions

--- R/mean_95ci.R
+++ R/mean_95ci.R
@@ -1,4 +1,15 @@
-# display result in "mean (lci, uci)"
+#' Display result in "mean (lci, uci)"
+#'
+#' @param mean Mean value
+#' @param lci Lower 95% CI
+#' @param uci Upper 95% CI
+#' @param dig Desired number of digits for display
+#'
+#' @return A string in the form of "mean (lci, uci)"
+#' @export
+#'
+#' @examples
+#' mean_95ci(1.02, 0.86, 1.21, 2)
 mean_95ci <- function(mean, lci, uci, dig){
   out <- paste0(formatC(mean, digits = dig, format = "f", drop0trailing = F), 
                 " (", formatC(lci, digits = dig, format = "f", drop0trailing = F), 
--- R/pct.R
+++ R/pct.R
@@ -1,4 +1,13 @@
-# display result in percentages
+#' Display results in percentages
+#'
+#' @param x numeric of any length
+#'
+#' @return The same numeric with decimal place moved 2 digits to the right.
+#' @export
+#'
+#' @examples
+#' x <- 0.8612
+#' pct(x)
 pct <- function(x){
   out <- formatC(x * 100, digits = 1, format = "f")
   return(out)
